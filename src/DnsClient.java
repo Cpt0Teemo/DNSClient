15cc709a502db46b718689badd3338b8c27b2a26
Cpt0Teemo <ypoulmarck@gmail.com>
2020-02-10 15:12:25 -0500

    Added retries and timeouts

--- src/DnsClient.java
+++ src/DnsClient.java
@@ -23,31 +23,47 @@ class DnsClient{
   private static void sendUdpRequest(DnsRequest request)
   {
     DatagramSocket clientSocket = null;
+    byte[] sendData = null;
+    byte[] receiveData = new byte[1024];
+
+
     try {
-      InetAddress IPAddress = InetAddress.getByAddress(request.getServerIpBytes());
-      clientSocket = new DatagramSocket();
-
-      byte[] sendData = request.createDnsRequest();
-      byte[] receiveData = new byte[1024];
-
-      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, request.getPort());
-
-      clientSocket.send(sendPacket);
-
-      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-
-      clientSocket.receive(receivePacket);
-
-      var response = new DnsResponse(receivePacket.getData());
-      var truth = response.checkResponseWithRequest(request);
-
-      System.out.println("FROM SERVER: " + truth );
-      clientSocket.close();
-    }catch(Exception e)
+      sendData = request.createDnsRequest();
+    }
+    catch (Exception e)
     {
       System.out.println(e.getMessage());
-    }finally {
-      if(clientSocket != null && !clientSocket.isClosed()) clientSocket.close();
+    }
+
+    var retries = request.getMax_retries();
+    while(retries > 0)
+    {
+      try {
+        InetAddress IPAddress = InetAddress.getByAddress(request.getServerIpBytes());
+        clientSocket = new DatagramSocket();
+        clientSocket.setSoTimeout(request.getTimeout() * 1000);
+
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, request.getPort());
+
+        clientSocket.send(sendPacket);
+
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+        clientSocket.receive(receivePacket);
+
+
+        var response = new DnsResponse(receivePacket.getData());
+        var truth = response.checkResponseWithRequest(request);
+
+        System.out.println("FROM SERVER: " + truth);
+        clientSocket.close();
+        break;
+      } catch (Exception e) {
+        System.out.println(e.getMessage());
+        retries--;
+      } finally {
+        if (clientSocket != null && !clientSocket.isClosed()) clientSocket.close();
+      }
     }
   }
 }
